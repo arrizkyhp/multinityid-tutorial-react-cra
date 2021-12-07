91e765c7e09311cc44d0a87d501f0bc22fab86f7
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-12-07 22:04:38 +0700

    todolist done ✔

--- src/components/Todo/TodoList/index.jsx
+++ src/components/Todo/TodoList/index.jsx
@@ -4,24 +4,42 @@ import { TodosContext } from "../../Todo";
 import { useContext } from 'react';
 
 export default function TodoList({ todosAll }) {
-    const { todos, activity } = useContext(TodosContext);
+    const { todos, activity, edit } = useContext(TodosContext);
     const [ todosValue, setTodosValue ] = todos;
     const [ activityValue, setActivityValue ] = activity;
+    const [ editValue, setEditValue ] = edit;
 
     const hapusHandler = (todoId) => {
         const filteredTodos = todosAll.filter( todo => {
             return todo.id !== todoId;
         })
           setTodosValue(filteredTodos);
+          setActivityValue('');
+          setEditValue('');
     }
 
-    const ubahHandler = (todoId) => {
-          const filteredTodos = todosAll.filter((todo) => {
-            return todo.id === todoId;
-          });
+    const ubahHandler = (todo) => {
+          const newActivity = todo.activity;
 
-          const newActivity = filteredTodos[0].activity;
+          console.log(todo);
           setActivityValue(newActivity);
+          setEditValue(todo);
+    }
+
+    const doneTodoHandler = (todo) => {
+      const updatedTodo = {
+        ...todo,
+        done: !todo.done
+      }
+
+       const editTodoIndex = todosValue.findIndex((currentTodo) => {
+         return currentTodo.id === todo.id;
+       });
+
+         const updatedTodos = [...todosValue];
+         updatedTodos[editTodoIndex] = updatedTodo;
+
+        return setTodosValue(updatedTodos);
 
     }
 
@@ -33,9 +51,11 @@ export default function TodoList({ todosAll }) {
                 return (
                   <div className="todolist" key={`todo-${todo.id}}`}>
                     <li>
+                      <input type="checkbox" checked={todo.done} onChange={doneTodoHandler.bind(this, todo)}/>
                       {todo.activity}
+                      {todo.done ? ' selesai' : ' belum selesai'}
                       <div className="button-action">
-                        <Button isSecondary onClick={ubahHandler.bind(this, todo.id)}>Ubah</Button>
+                        <Button isSecondary onClick={ubahHandler.bind(this, todo)}>Ubah</Button>
                         <Button isDelete onClick={hapusHandler.bind(this, todo.id)}>Hapus</Button>
                       </div>
                     </li>
--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -8,42 +8,95 @@ export const TodosContext = createContext({
 
 export default function Todo() {
     const [activity, setActivity] = useState("");
+    const [edit, setEdit] = useState({});
     const [todos, setTodos] = useState([]);
 
-    const value = { todos: [todos, setTodos], activity: [activity, setActivity] };
+    const [message, setMessage ] = useState('');
+
+    const value = {
+        todos: [todos, setTodos],
+        activity: [activity, setActivity],
+        edit: [edit, setEdit]
+    };
 
     const generateId = () => {
         return Date.now();
     }
 
-    const tambahHandler = (event) => {
+    const simpanHandler = (event) => {
         event.preventDefault();
+
+        if (!activity) {
+            return setMessage('Nama Aktifitas tidak boleh kosong!');
+        }
+
+        setMessage("");
+
+        if(edit.id) {
+
+            if (!activity) {
+              return setMessage("Nama Aktifitas tidak boleh kosong!");
+            }
+
+            const updatedTodo = {
+                ...edit,
+                activity,
+                done: todos[0].done,
+            }
+
+            const editTodoIndex = todos.findIndex( todo => {
+                return todo.id ===  edit.id
+            })
+
+            const updatedTodos = [
+                ...todos,
+            ]
+
+            updatedTodos[editTodoIndex] = updatedTodo;
+
+            setTodos(updatedTodos);
+
+            return batalUbahHandler();
+        }
 
         setTodos([...todos, {
             id: generateId(),
             activity: activity,
+            done: false,
         }]);
         setActivity('');
     };
 
+    const batalUbahHandler = () => {
+        setActivity("");
+        setEdit({});
+        setMessage("");
+
+    }
+
     return (
-        <section className="todolist">
+      <section className="todolist">
         <h1 className="header-todolist">Simple Todo List</h1>
-        <form onSubmit={tambahHandler}>
-            <input
+        {message && <p style={{ margin: 0 }}>{message}</p>}
+        <form onSubmit={simpanHandler}>
+          <input
             type="text"
             placeholder="Nama Aktifitas"
             value={activity}
             onChange={(event) => {
-                setActivity(event.target.value);
+              setActivity(event.target.value);
             }}
-            />
-            <Button isPrimary>Tambah</Button>
+          />
+          <Button isPrimary>{edit.id ? "Simpan Perubahan" : "Tambah"}</Button>
+          { edit.id &&
+          <Button isSecondary onClick={batalUbahHandler}>Batal Ubah</Button>
+          }
         </form>
-        <TodosContext.Provider value={value}>
-            <TodoList todosAll={todos} />
-        </TodosContext.Provider>
-
-        </section>
+        { todos.length > 0 ? (
+            <TodosContext.Provider value={value}>
+                <TodoList todosAll={todos} />
+            </TodosContext.Provider>
+        ) : <p>belum ada todo</p>}
+      </section>
     );
 }
